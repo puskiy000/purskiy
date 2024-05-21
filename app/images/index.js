07f69eb24d271a25d72319b3238b8512a4f18473
ppurskiy <ppurskiy@gmail.com>
2024-05-21 16:36:14 -0700

    react-typescript-cheatsheets

--- app/images/index.js
+++
@@ -1,9 +0,0 @@
-import hero from './hero.png';
-import about from './about.jpg';
-import Bcgame from './8.png';
-import threedmap from './6.png';
-import SportUniformConfi from './1.png';
-import tictactoe from './tictac.png';
-import Heroforge from './3.png';
-import MetaMap from './4.png';
-export { hero, about, Bcgame, threedmap, SportUniformConfi, tictactoe,Heroforge,MetaMap};
